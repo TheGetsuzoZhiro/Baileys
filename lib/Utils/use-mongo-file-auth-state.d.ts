18fee9f278aed5ef985e2819e70e69929770f906
The Zhiro Dev` <108138548+TheGetsuzoZhiro@users.noreply.github.com>
2025-09-10 15:16:57 +0700

    Add files via upload

---
+++ lib/Utils/use-mongo-file-auth-state.d.ts
@@ -0,0 +1,5 @@
+import { AuthenticationState } from '../Types'
+export declare const useMongoFileAuthState: (collection: string) => Promise<{
+state: AuthenticationState
+saveCreds: () => Promise<void>
+}>
